cf4ccc18f85df7ade06098a0b036e7a4f9a568a5
Serhii Larionov <serhiilarionov@gmail.com>
2016-07-07 16:27:39 +0300

    added migrations for access token and acl

--- server/migrations/pgsql/user.js
+++ server/migrations/pgsql/user.js
@@ -2,18 +2,24 @@ module.exports = {
   up: function (dataSource, next) {
     dataSource.autoupdate('User', function (err) {
       if (err) throw err;
+      dataSource.autoupdate('AccessToken', function (err) {
+        if (err) throw err;
+        dataSource.autoupdate('ACL', function (err) {
+          if (err) throw err;
 
-      dataSource.models.User.create([
-        {
-          "email": "test@test.test",
-          'password': "test"
-        },
-        {
-          'email': 'admin@admin.com',
-          'password': '123321'
-        }
-      ], next);
-    });
+          dataSource.models.User.create([
+            {
+              "email": "test@test.test",
+              'password': "test"
+            },
+            {
+              'email': 'admin@admin.com',
+              'password': '123321'
+            }
+          ], next);
+        })
+      });
+    })
   },
   down: function (dataSource, next) {
     dataSource.models.User.destroyAll([], next);
