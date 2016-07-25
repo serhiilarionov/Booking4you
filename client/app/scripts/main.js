6d10f37a2a871ab608b1112f328a5b61a6b01909
Dmitriy <dmitriy.ost1@gmail.com>
2016-07-25 12:27:16 +0300

    Change location strategy

--- client/app/scripts/main.js
+++ client/app/scripts/main.js
@@ -1,9 +1,11 @@
 "use strict";
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var common_1 = require('@angular/common');
 var app_component_1 = require('./app.component');
 var app_routes_1 = require('./app.routes');
 platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [
-    app_routes_1.appRouterProviders
+    app_routes_1.appRouterProviders,
+    { provide: common_1.LocationStrategy, useClass: common_1.HashLocationStrategy }
 ]);
 console.log('HELLOsdf as');
 //# sourceMappingURL=main.js.map
--- client/app/scripts/main.ts
+++ client/app/scripts/main.ts
@@ -1,8 +1,11 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { AppComponent } from './app.component';
 import { appRouterProviders }   from './app.routes';
+
 bootstrap(AppComponent, [
-  appRouterProviders
+  appRouterProviders,
+  {provide: LocationStrategy, useClass: HashLocationStrategy}
 ]);
 
 console.log('HELLOsdf as');
