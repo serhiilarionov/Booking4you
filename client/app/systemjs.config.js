847ef4156fc38702e175c567726a805d590a8905
Dmitriy <dmitriy.ost1@gmail.com>
2016-07-25 17:26:22 +0300

    Include jquery. Normal hero component functionality

--- client/app/systemjs.config.js
+++ client/app/systemjs.config.js
@@ -5,9 +5,9 @@
 (function(global) {
   // map tells the System loader where to look for things
   var map = {
-    'app':                        'scripts', // 'dist',
-    '@angular':                   'node_modules/@angular',
-    'rxjs':                       'node_modules/rxjs'
+    'app': 'scripts', // 'dist',
+    '@angular': 'node_modules/@angular',
+    'rxjs': 'node_modules/rxjs'
   };
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
