5566cc2eb0c1d05db3bc4b0d2957019ac1b3a528
Serhii Larionov <serhiilarionov@gmail.com>
2016-07-28 11:45:57 +0300

    moved data from companyLocation to company

--- server/migrations/pgsql/company.js
+++ server/migrations/pgsql/company.js
@@ -10,7 +10,14 @@ module.exports = {
           'title': 'Автомойка «Экспресс»',
           'categoryId': 1,
           'desc': 'EXPRESS МОЙКА 25грн. 10мин. Каждая 10 мойка БЕСПЛАТНО ХИМЧИСТКА КОВРОВ 15грн.М2 ПОЛИРОВКА КУЗОВА Комплекс для TAXI 35грн.',
-          'active': true
+          'active': true,
+          'point': "33.421607, 47.936635",
+          'buildingId': 1,
+          'streetId': 1,
+          'districtId': 5,
+          'cityId': 1,
+          'room': '',
+          'locationDetail': ''
         },
         {
           'id': 2,
@@ -18,7 +25,14 @@ module.exports = {
           'title': 'Автоцентр «CITIАВТО»',
           'categoryId': 2,
           'desc': 'Услуги автомойки: бесконтактная мойка кузова, удаление битумных и других пятен любой сложности, широкий выбор автохимии по уходу и защите ЛКП и деталей автомобиля, химчистку салона, бесконтактную мойку моторного отсека специальными средствами.',
-          'active': true
+          'active': true,
+          'point': "33.377237, 47.888798",
+          'buildingId': 2,
+          'streetId': 2,
+          'districtId': 1,
+          'cityId': 1,
+          'room': '',
+          'locationDetail': 'район Песочной'
         },
         {
           'id': 3,
@@ -26,7 +40,14 @@ module.exports = {
           'title': 'Манго',
           'categoryId': 21,
           'desc': 'Мы рады предложить широкий спектр услуг по уходу за Вашим автомобилем: профессиональная мойка авто, химчистка салона, предпродажная подготовка кузова, восстановление цвета кузова, удаление царапин, восстановление резины, полировка фар, стекол, экспресс-мойка 3-4 минуты!',
-          'active': true
+          'active': true,
+          'point': "33.437559, 47.995350",
+          'buildingId': 3,
+          'streetId': 3,
+          'districtId': 3,
+          'cityId': 1,
+          'room': '',
+          'locationDetail': ''
         },
         {
           'id': 4,
@@ -34,7 +55,14 @@ module.exports = {
           'title': 'Автомойка "5555"',
           'categoryId': 21,
           'desc': 'Автомойка "5555" в Кривом Роге оказывает полный спектр услуг по мойке и химчистке автомобилей: бесконтактная мойка, химчистка салона, стирка ковров, мойка двигателя, чернение резины, полировка AMWAY. Время роботы: круглосуточно.',
-          'active': true
+          'active': true,
+          'point': "33.286898, 47.885698",
+          'buildingId': 4,
+          'streetId': 4,
+          'districtId': 7,
+          'cityId': 1,
+          'room': '',
+          'locationDetail': ''
         },
         {
           'id': 5,
@@ -42,7 +70,14 @@ module.exports = {
           'title': 'Автомойка "Немо"',
           'categoryId': 21,
           'desc': 'Мойка, химчистка.',
-          'active': true
+          'active': true,
+          'point': "33.321911, 47.907665",
+          'buildingId': 5,
+          'streetId': 5,
+          'districtId': 7,
+          'cityId': 1,
+          'room': '',
+          'locationDetail': ''
         },
         {
           'id': 6,
@@ -50,7 +85,14 @@ module.exports = {
           'title': 'Автомойка-кафе "Elvis"',
           'categoryId': 21,
           'desc': 'Автомойка-кафе "Elvis" в Кривом Роге - с радостью,предоставит вам самые качественные услуги: мойка вашего авто,любых видов загрязнения;химчистка салона; полировка кузова и т.д. Так же к вашим услугам работает шинамонтаж и уютное кафе на втором этаже комплекса.',
-          'active': true
+          'active': true,
+          'point': "33.312606, 47.907304",
+          'buildingId': 6,
+          'streetId': 5,
+          'districtId': 7,
+          'cityId': 1,
+          'room': '',
+          'locationDetail': ''
         },
         {
           'id': 7,
@@ -58,7 +100,14 @@ module.exports = {
           'title': 'Автомойка и ремонтные боксы для автомобилей',
           'categoryId': 21,
           'desc': 'Автомойка и ремонтные боксы для автомобилей. СТО',
-          'active': true
+          'active': true,
+          'point': "33.426990, 47.970498",
+          'buildingId': 7,
+          'streetId': 6,
+          'districtId': 5,
+          'cityId': 1,
+          'room': '',
+          'locationDetail': ''
         },
         {
           'id': 8,
@@ -66,7 +115,14 @@ module.exports = {
           'title': 'Модена Авто',
           'categoryId': 21,
           'desc': 'Автомойка, хим чистка.',
-          'active': true
+          'active': true,
+          'point': "35.046775, 48.432023",
+          'buildingId': 8,
+          'streetId': 7,
+          'districtId': 10,
+          'cityId': 2,
+          'room': '',
+          'locationDetail': ''
         },
         {
           'id': 9,
@@ -74,7 +130,14 @@ module.exports = {
           'title': 'Автоцентр «Аэлита». Mitsubishi Motors',
           'categoryId': 21,
           'desc': 'Здесь представлен полный модельный ряд автомобилей, крупный сервисный центр оборудован по последнему слову техники. Широкий спектр услуг: индивидуальный тест-драйв, продажа в кредит, страхование, трейд-ин (обмен старого автомобиля на новый с доплатой), лизинг. ВРЕМЯ РАБОТЫ: 9:00-18:00',
-          'active': true
+          'active': true,
+          'point': "34.805495, 48.430485",
+          'buildingId': 9,
+          'streetId': 8,
+          'districtId': 12,
+          'cityId': 2,
+          'room': '',
+          'locationDetail': ''
         },
         {
           'id': 10,
@@ -101,7 +164,14 @@ module.exports = {
           '-установка противоугонных замков MUL-T-LOCK; ' +
           '-установка автосигнализаций; ' +
           '-обесшумливание салона; … и многое другое.',
-          'active': true
+          'active': true,
+          'point': "35.122061, 48.494692",
+          'buildingId': 10,
+          'streetId': 9,
+          'districtId': 15,
+          'cityId': 2,
+          'room': '',
+          'locationDetail': ''
         },
         {
           'id': 11,
@@ -109,7 +179,14 @@ module.exports = {
           'title': 'Мерседес центр',
           'categoryId': 21,
           'desc': 'Официальный дилер Mercedes-Benz. Ремонтные и сервисные работы автомобилей Mercedes-Benz. Любая продукция для автомобилей Mercedes-benz.',
-          'active': true
+          'active': true,
+          'point': "35.016339, 48.459405",
+          'buildingId': 11,
+          'streetId': 10,
+          'districtId': 12,
+          'cityId': 2,
+          'room': '',
+          'locationDetail': ''
         },
         {
           'id': 12,
@@ -117,7 +194,14 @@ module.exports = {
           'title': 'АИС Автоцентр Днепр',
           'categoryId': 21,
           'desc': 'Автоцентр, мойка, СТО.',
-          'active': true
+          'active': true,
+          'point': "35.074855, 48.450705",
+          'buildingId': 12,
+          'streetId': 11,
+          'districtId': 10,
+          'cityId': 2,
+          'room': '',
+          'locationDetail': ''
         },
         {
           'id': 13,
@@ -125,7 +209,14 @@ module.exports = {
           'title': 'Автомойка Акваматик',
           'categoryId': 21,
           'desc': 'Автоматическая автомойка',
-          'active': true
+          'active': true,
+          'point': "35.027667, 48.478605",
+          'buildingId': 13,
+          'streetId': 12,
+          'districtId': 12,
+          'cityId': 2,
+          'room': '',
+          'locationDetail': ''
         },
         {
           'id': 14,
@@ -133,7 +224,14 @@ module.exports = {
           'title': 'ЛУКОЙЛ',
           'categoryId': 21,
           'desc': 'Замена масел, автомойка',
-          'active': true
+          'active': true,
+          'point': "30.548981, 50.344374",
+          'buildingId': 14,
+          'streetId': 13,
+          'districtId': 16,
+          'cityId': 3,
+          'room': '',
+          'locationDetail': ''
         },
         {
           'id': 15,
@@ -141,7 +239,14 @@ module.exports = {
           'title': 'Автомойка',
           'categoryId': 21,
           'desc': 'Проф.полировка кузова, полная химчистка салона, расконцервация кузова, передпродажная подготовка автомобиля, мойка двигателя.',
-          'active': true
+          'active': true,
+          'point': "30.336987, 50.49294",
+          'buildingId': 15,
+          'streetId': 14,
+          'districtId': 23,
+          'cityId': 3,
+          'room': '',
+          'locationDetail': ''
         },
         {
           'id': 16,
@@ -149,7 +254,14 @@ module.exports = {
           'title': 'Блеск-авто',
           'categoryId': 21,
           'desc': 'Удаление царапин и других дефектов. Восстановительная и защитная полировка. Профессиональный косметический уход за автомобилем. Полировка фар. Химчистка.',
-          'active': true
+          'active': true,
+          'point': "30.49466, 50.476915",
+          'buildingId': 16,
+          'streetId': 15,
+          'districtId': 22,
+          'cityId': 3,
+          'room': '',
+          'locationDetail': ''
         },
         {
           'id': 17,
@@ -157,7 +269,14 @@ module.exports = {
           'title': 'Пронта',
           'categoryId': 21,
           'desc': 'Автомойка, полировка, химчистка.',
-          'active': true
+          'active': true,
+          'point': "30.508665, 50.528409",
+          'buildingId': 17,
+          'streetId': 16,
+          'districtId': 20,
+          'cityId': 3,
+          'room': '',
+          'locationDetail': ''
         },
         {
           'id': 18,
@@ -165,7 +284,14 @@ module.exports = {
           'title': 'Паровая автомойка',
           'categoryId': 21,
           'desc': 'Комплексная очистка салона автомобиля паром - это передовая авто моечная технология. Одна из главных ее преимуществ в том, что чистка паром позволяет быстро и эффективно очистить автомобиль от загрязнений самого разного рода. Комплексное чистки автомобиля паром, это инновационная, безопасная, эффективная технология очистки.',
-          'active': true
+          'active': true,
+          'point': "30.587725, 50.477161",
+          'buildingId': 18,
+          'streetId': 17,
+          'districtId': 19,
+          'cityId': 3,
+          'room': '',
+          'locationDetail': ''
         }
       ], next);
     });
