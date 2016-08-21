79089f14eaaf155b3ae63fb49447237d1ac762ec
Eugene Telyatnik <eugene.telyatnik@gmail.com>
2016-08-22 00:12:09 +0300

    Add Core SMS Status and Notify

--- common/models/core.js
+++ common/models/core.js
@@ -1,6 +1,11 @@
-module.exports = function(Core) {
+module.exports = function (Core) {
 
-    Core.phoneVerification = function(phone, cb) {
+    /**
+     * Phone Verification
+     * @param phone
+     * @param cb
+     */
+    Core.phoneVerification = function (phone, cb) {
 
         var body = {
             "phone": phone,
@@ -8,9 +13,9 @@ module.exports = function(Core) {
             "userId": 2
         };
 
-        var path = "2f96f65910c5e3af4b0dda1dcfa4322068e17d0a";
+        var path = "public/156034/2f96f65910c5e3af4b0dda1dcfa4322068e17d0a";
 
-        Core.run(path, body, function(err, result) {
+        Core.run(path, body, function (err, result) {
             cb(null, result);
         });
     };
@@ -22,4 +27,77 @@ module.exports = function(Core) {
             returns: {arg: 'result', type: 'object'}
         }
     );
+
+    /**
+     * Sms Status from provider
+     * @param id
+     * @param phone
+     * @param status
+     * @param time
+     * @param ts
+     * @param err
+     * @param cb
+     */
+    Core.smsStatus = function (id, phone, status, time, ts, err, cb) {
+
+        var body = {
+            "task_id": id,
+            "phone": phone,
+            "status": status
+        };
+
+        var path = "callback/57b97a54f6c3760bc3b516fc/25940f33b562bfd33db4db5af324ec74413b117f";
+
+        Core.run(path, body, function (err, result) {
+            cb(null, result);
+        });
+
+    };
+
+    Core.remoteMethod(
+        'smsStatus',
+        {
+            accepts: [
+                {arg: 'id', type: 'string', required: true},
+                {arg: 'phone', type: 'string', required: true},
+                {arg: 'status', type: 'string', required: true},
+                {arg: 'time', type: 'string'},
+                {arg: 'ts', type: 'string'},
+                {arg: 'err', type: 'string'}
+            ],
+            returns: {arg: 'result', type: 'object'}
+        }
+    )
+
+    /**
+     * Notify user
+     * @param name
+     * @param type
+     * @param process
+     * @param data
+     * @param cb
+     */
+    Core.notify = function (name, type, process, data, cb) {
+        console.log(name);
+        console.log(type);
+        console.log(process);
+        console.log(data);
+
+        cb(null, data);
+    };
+
+    Core.remoteMethod(
+        'notify',
+        {
+            accepts: [
+                {arg: 'name', type: 'string', required: true},
+                {arg: 'type', type: 'string', required: true},
+                {arg: 'process', type: 'string', required: true},
+                {arg: 'data', type: 'object'}
+            ],
+            returns: {arg: 'result', type: 'object'}
+        }
+    )
+
+
 };
