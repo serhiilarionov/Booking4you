f77af2a655abfa6462290e4869dcdb49d212cdc8
Dmitriy <dmitriy.ost1@gmail.com>
2016-08-31 19:30:20 +0300

    Registration functionality

--- client/app/scripts/components/auth/registration-form.conponent.js
+++ client/app/scripts/components/auth/registration-form.conponent.js
@@ -8,14 +8,23 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 var core_1 = require('@angular/core');
+var router_1 = require('@angular/router');
 var forms_1 = require('@angular/forms');
 var index_1 = require('../../shared/index');
 var RegistrationFormComponent = (function () {
-    function RegistrationFormComponent(formBuilder) {
+    function RegistrationFormComponent(formBuilder, clientApi, router, authRedirect, broadcaster) {
         this.formBuilder = formBuilder;
+        this.clientApi = clientApi;
+        this.router = router;
+        this.authRedirect = authRedirect;
+        this.broadcaster = broadcaster;
         this.submitted = false;
         this.passwordMinLength = index_1.PASSWORD_MIN_LENGTH;
+        this.data = null;
         this.registrationForm = this.formBuilder.group({
             email: ['', forms_1.Validators.compose([
                     forms_1.Validators.required,
@@ -35,14 +44,33 @@ var RegistrationFormComponent = (function () {
     RegistrationFormComponent.prototype.onSubmit = function () {
         this.submitted = true;
         if (this.registrationForm.valid) {
+            this.data = {
+                email: this.registrationForm.value.email,
+                password: this.registrationForm.value.passwords.password
+            };
+            this.register(this.data);
         }
+    };
+    RegistrationFormComponent.prototype.register = function (userData) {
+        var _this = this;
+        this.clientApi.create(userData).subscribe(function () {
+            // login on success
+            _this.clientApi.login(_this.data).subscribe(function () {
+                _this.broadcaster.emit(index_1.EventTypes.LOGGED_IN);
+                _this.router.navigate([_this.authRedirect.getRedirectUrl()]);
+            });
+        }, function (error) {
+            console.log(error);
+        });
     };
     RegistrationFormComponent = __decorate([
         core_1.Component({
             selector: 'registration-form',
             templateUrl: 'scripts/components/auth/registration-form.component.html'
-        }), 
-        __metadata('design:paramtypes', [forms_1.FormBuilder])
+        }),
+        __param(3, core_1.Inject(index_1.AuthRedirectService)),
+        __param(4, core_1.Inject(index_1.Broadcaster)), 
+        __metadata('design:paramtypes', [forms_1.FormBuilder, index_1.ClientApi, router_1.Router, index_1.AuthRedirectService, index_1.Broadcaster])
     ], RegistrationFormComponent);
     return RegistrationFormComponent;
 }());
--- client/app/scripts/components/auth/registration-form.conponent.ts
+++ client/app/scripts/components/auth/registration-form.conponent.ts
@@ -1,6 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { EMAIL_REGEX, PASSWORD_MIN_LENGTH, CustomValidators } from '../../shared/index';
+import {
+  EMAIL_REGEX,
+  PASSWORD_MIN_LENGTH,
+  CustomValidators,
+  ClientApi,
+  Broadcaster,
+  EventTypes,
+  AuthRedirectService
+} from '../../shared/index';
 
 @Component({
   selector: 'registration-form',
@@ -11,8 +20,15 @@ export class RegistrationFormComponent {
   registrationForm: FormGroup;
   submitted: boolean = false;
   passwordMinLength = PASSWORD_MIN_LENGTH;
+  private data: any = null;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private clientApi: ClientApi,
+    private router: Router,
+    @Inject(AuthRedirectService) private authRedirect: AuthRedirectService,
+    @Inject(Broadcaster) private broadcaster: Broadcaster<string>
+  ) {
     this.registrationForm = this.formBuilder.group({
       email: ['', Validators.compose([
         Validators.required,
@@ -30,11 +46,27 @@ export class RegistrationFormComponent {
     });
   }
 
-  onSubmit() {
+  public onSubmit() {
     this.submitted = true;
 
     if (this.registrationForm.valid) {
-      // TODO: registration functionality
+      this.data = {
+        email: this.registrationForm.value.email,
+        password: this.registrationForm.value.passwords.password
+      };
+      this.register(this.data);
     }
+  }
+
+  private register(userData) {
+    this.clientApi.create(userData).subscribe(() => {
+      // login on success
+      this.clientApi.login(this.data).subscribe(() => {
+        this.broadcaster.emit(EventTypes.LOGGED_IN);
+        this.router.navigate([this.authRedirect.getRedirectUrl()]);
+      });
+    }, (error) => {
+      console.log(error);
+    });
   }
 }
