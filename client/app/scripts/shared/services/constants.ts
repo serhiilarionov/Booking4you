111b1393c34de27be24cf51f0dcd504b9a2cfede
Dmitriy <dmitriy.ost1@gmail.com>
2016-09-17 19:55:00 +0300

    Dev server for api

--- client/app/scripts/shared/services/constants.ts
+++ client/app/scripts/shared/services/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'http://dev.booking4you.com.ua';
 export const API_VERSION = 'api';
 export const EMAIL_REGEX: string = '^(([a-zA-Z]|[0-9])|([-]|[_]|[.]))+[@](([a-zA-Z0-9])|([-])){2,63}[.](([a-zA-Z0-9]){2,63})+$';
 export const PASSWORD_MIN_LENGTH: number = 5;
