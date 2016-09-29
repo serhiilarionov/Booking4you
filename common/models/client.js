2ecc544a1f92bf811b049943a7a00ade5be8b95f
Serhii Larionov <serhiilarionov@gmail.com>
2016-09-29 12:35:41 +0300

    added method geoLogin for receiving city after authorization

--- common/models/client.js
+++ common/models/client.js
@@ -26,4 +26,44 @@ module.exports = function(Client) {
   Client.disableRemoteMethod('__get__accessTokens', false);
   Client.disableRemoteMethod('__updateById__accessTokens', false);
 
+  /**
+   * Login with geolocation
+   * @param credentials
+   * @param cb
+   */
+  Client.geoLogin = function (credentials, cb) {
+    var result = {};
+    Client.login({email: credentials.email, password: credentials.password}, function (err, token) {
+      result = token;
+
+      var ds = Client.dataSource;
+      var sql = "SELECT city.id, city.nameru from city where " +
+        "ST_GeomFromText('POINT(' || " +
+        "replace('" + credentials.latlng + "', ',', ' ') || ')', 4326) " +
+        "&& ST_MakeEnvelope(cast(split_part(city.bound, ', ', 1) as FLOAT), cast(split_part(city.bound, ', ', 2) as FLOAT)," +
+        " cast(split_part(city.bound, ', ', 3) as FLOAT), cast(split_part(city.bound, ', ', 4) as FLOAT));";
+
+      ds.connector.query(sql, [], function (err, city) {
+        if (err) console.error(err);
+        if(city.length) {
+          result.cityName = city[0].nameru;
+          result.cityId = city[0].id;
+        }
+        cb(err, result);
+      });
+    });
+
+  };
+
+  Client.remoteMethod(
+    'geoLogin',
+    {
+      accepts: [
+        {arg: 'credentials', type: 'object', required: true}
+      ],
+      returns: {arg: 'result', type: 'object'}
+    }
+  )
+
 };
+
