4ded279d8bfd3869220d468921330e4129edc7bb
Dmitriy <dmitriy.ost1@gmail.com>
2016-10-05 21:32:56 +0300

    Small fixes

--- client/app/scripts/shared/services/index.ts
+++ client/app/scripts/shared/services/index.ts
@@ -2,19 +2,16 @@ import { SidebarService } from './sidebar.service';
 import { AuthGuardService } from './auth-guard.service';
 import { AuthService } from './auth.service';
 import { AuthRedirectService } from './auth-redirect.service';
-// import { InfoboxLoader } from './infobox-loader.service';
 
 export { SidebarService } from './sidebar.service';
 export { AuthGuardService } from './auth-guard.service';
 export { AuthService } from './auth.service';
 export { AuthRedirectService } from './auth-redirect.service';
-// export { InfoboxLoader } from './infobox-loader.service';
 export * from './constants';
 
 export const Services: Array<any> = [
   SidebarService,
   AuthGuardService,
   AuthService,
-  AuthRedirectService,
-  // InfoboxLoader
+  AuthRedirectService
 ];
